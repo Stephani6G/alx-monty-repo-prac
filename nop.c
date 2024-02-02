e0f756be19e1350dd974f4a08c556f54f8182839
Stephani6G <stephaniegloria283@gmail.com>
2024-02-02 00:14:53 -0800

    monty

---
+++ nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_nop- funtion that does nothing
+ * @head: stack head
+ *
+ * @counter: line_number
+ *
+ * Return: no return
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
